43bdb8c6ba65f75c5fc01e5e506cab07972907bc
Krisztian Kiss <kisskrisztian215@gmail.com>
2023-04-05 01:33:02 +0200

    Added AddFruits method to update Each Fruit's property

--- js/src/fruitgame.js
+++ js/src/fruitgame.js
@@ -205,4 +205,17 @@ export class game {
                }
              }
         }
+        addFruits(){
+            this.apple = new fruit(appleImg,'apple', appleTop, appleBottom, appleSplash,'right',300,'minSpin', greenParticle,25,120,40,1500,120,120);
+            this.melon = new fruit(melonImg, 'melon', melonTop, melonBottom, melonSplash, 'right', 300, 'minSpin', redParticle,25,120,40,1500,120,120);
+            this.strawberry = new fruit(strawberryImg, 'strawberry', strawberryTop, strawberryBottom, strawberrySplash,'right',155,'minSpin',redParticle,25,120,40,1500,120,120);
+            this.bomb = new fruit(bombImg, 'bomb',null,null,null,'right',300,'minSpin',starParticle,40,700,150,3000,120,120);
+            this.dragonFruit = new fruit(dragonFruitImg, 'dragonfruit', dragonFruitTop, dragonFruitBottom, dragonFruitSplash,'right',155,'minSpin',redParticle,25,120,40,1500,110,120);
+            this.coconut = new fruit(coconutImg, 'coconut', coconutTop, coconutBottom, coconutSplash,'right',300,'minSpin',whiteParticle,25,120,40,1500,120,110);
+            this.peach = new fruit(peachImg, 'peach',peachTop, peachBottom, peachSplash, 'right', 300, 'minSpin', darkyellowParticle,25,120,40,1500,120,120);
+            this.pineapple = new fruit(pineAppleImg, 'pineapple', pineAppleTop, pineappleBottom, pineAppleSplash,'left', 300, 'maxSpin', lightyellowParticle,25,120,40,1500,140,180);
+            this.freezebanana = new fruit(freezeBananaImg, 'freezebanana', freezeBananaTop, freezebananaBottom,null,'left',300,'maxSpin', iceParticle,25,120,40,1500,90,140);
+            this.banana = new fruit(bananaImg, 'banana', bananaTop, bananaBottom,bananaSplash, 'right',300,'maxSpin',lightyellowParticle,25,120,40,1500,90,140);
+            this.starfruit = new fruit(starFruitImg, 'starfruit', starFruitTop, starFruitBottom,appleSplash,'right',300,'minSpin',starFruitParticle,25,120,40,1500,120,120);
+        }
 }
